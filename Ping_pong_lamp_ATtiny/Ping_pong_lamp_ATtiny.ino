b46be1f9b92a6d6d65cdcc2f7dd279ea0944c929
Samuel Yamoah <sya57@uclive.ac.nz>
2016-07-11 02:43:15 +1200

    Added a pulse of one colour. Note Slight bug in exiting state

--- Ping_pong_lamp_ATtiny/Ping_pong_lamp_ATtiny.ino
+++ Ping_pong_lamp_ATtiny/Ping_pong_lamp_ATtiny.ino
@@ -102,20 +102,40 @@ void toggle_funct()
   }
 
   else if (colourState == 5){
-     // fade in from min to max in increments of 5 points:
-    for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) { 
-      // sets the value (range from 0 to 255):
-      analogWrite(led3, fadeValue);         
-      // wait for 30 milliseconds to see the dimming effect    
-      delay(30);                            
-    } 
-    
-    // fade out from max to min in increments of 5 points:
-    for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) { 
-      // sets the value (range from 0 to 255):
-      analogWrite(led3, fadeValue);         
-      // wait for 30 milliseconds to see the dimming effect    
-      delay(30);                            
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+
+    while(1){
+      // fade in from min to max in increments of 5 points:
+      for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) { 
+        // sets the value (range from 0 to 255):
+        analogWrite(led3, fadeValue);         
+        // wait for 30 milliseconds to see the dimming effect    
+        delay(50);
+        if (digitalRead(button) ==HIGH){
+        colourState = 0;
+        break;
+       }                            
+      } 
+      
+      // fade out from max to min in increments of 5 points:
+      for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) { 
+        // sets the value (range from 0 to 255):
+        analogWrite(led3, fadeValue);         
+        // wait for 30 milliseconds to see the dimming effect    
+        delay(50);
+        if (digitalRead(button) ==HIGH){
+        colourState = 0;
+        break;
+       }                           
+      }
+       analogWrite(led3, 0);
+       delay(100);
+       if (digitalRead(button) ==HIGH){
+        colourState = 0;
+        break;
+       }
     }
   }
 }
