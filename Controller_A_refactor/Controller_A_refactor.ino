84510aa5008340f120821b77b5aa760c9c3532c1
Samuel Yamoah <Sam.Yamoah@hotmail.com>
2016-07-27 01:26:12 +1200

    tidied up Controller A from last A commit. Looping and existing need to be implimented

--- Controller_A_refactor/Controller_A_refactor.ino
+++ Controller_A_refactor/Controller_A_refactor.ino
@@ -1,8 +1,8 @@
 /*******************************************************************************
-@file     Controller_A.ino
+@file     Controller_A_refactor.ino
 @author   Samuel Yamoah
 @date     29.06.2016
-@modified 24.06.2016
+@modified 27.06.2016
 @brief    Main Code for Ping POng Ball Lamp. Each Ping Pong Ball will have an
           RGB LED (72 total). Program will have solid RED, GREEN, BLUE, WHITE,
           pulse WHITE & pulse RGB Spectrum.
@@ -48,90 +48,116 @@ void loop(){
   }
 }
 
+//Function selects the colour state
 void toggle_funct(){
-  //ALL OFF
-  if (colourState == 0){
-     digitalWrite(RED, LOW);
-     digitalWrite(GREEN, LOW);
-     digitalWrite(OUTPUT_A, LOW);
-     digitalWrite(OUTPUT_B, LOW);
+  switch(colourState){
+    case 0:
+      all_off();
+      break;
+      
+    case 1:
+      red();
+      break;
+
+    case 2:
+      green();
+      break;
+
+    case 3:
+      blue();
+      break;
+
+    case 4:
+      white();
+      break;
+
+    case 5:
+      white_pulse();
+      break;
+
+    case 6:
+      rgb();
+      break; 
+  }
+}
+
+
+//ALL LIGHTS OFF
+void all_off(){
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(OUTPUT_A, LOW);
+  digitalWrite(OUTPUT_B, LOW);
+}
+
+//RED LIGHT SOLID
+void red(){
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(OUTPUT_A, LOW);
+  digitalWrite(OUTPUT_B, LOW);
+}
+
+//GREEN LIGHT SOLID
+void green(){
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(OUTPUT_A, LOW);
+  digitalWrite(OUTPUT_B, LOW);
+}
+
+//BLUE LIGHT SOLID
+void blue(){
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(OUTPUT_A, HIGH);
+  digitalWrite(OUTPUT_B, LOW);
+}
+
+//WHITE LIGHT SOLID
+void white(){
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(OUTPUT_A, HIGH);
+  digitalWrite(OUTPUT_B, LOW);
+}
+
+//WHITE LIGHT PULSE
+void white_pulse(){
+  //bool quit = false;
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(OUTPUT_A, LOW);
+  digitalWrite(OUTPUT_B, HIGH);
+
+  // fade in from min to max in increments of 5 points:
+  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(RED, fadeValue);
+    analogWrite(GREEN, fadeValue);
+    // wait for 50 milliseconds to see the dimming effect
+    delay(50);
+ }
+
+  // fade out from max to min in increments of 5 points:
+  for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(RED, fadeValue);
+    analogWrite(GREEN, fadeValue);
+    // wait for 50 milliseconds to see the dimming effect
+    delay(50);
+
+    //if (digitalRead(BUTTON) == HIGH) quit = true;
   }
 
-  //RED
-  else if (colourState == 1){
-     digitalWrite(RED, HIGH);
-     digitalWrite(GREEN, LOW);
-     digitalWrite(OUTPUT_A, LOW);
-     digitalWrite(OUTPUT_B, LOW);
-  }
+  analogWrite(RED, 0);
+  analogWrite(GREEN, 0);
+  delay(100);
+}
 
-  //GREEN
-  else if (colourState == 2){
-     digitalWrite(RED, LOW);
-     digitalWrite(GREEN, HIGH);
-     digitalWrite(OUTPUT_A, LOW);
-     digitalWrite(OUTPUT_B, LOW);
-  }
-
-  //BLUE
-  else if (colourState == 3){
-     digitalWrite(RED, LOW);
-     digitalWrite(GREEN, LOW);
-     digitalWrite(OUTPUT_A, HIGH);
-     digitalWrite(OUTPUT_B, LOW);
-  }
-
-  //WHITE
-  else if (colourState == 4){
-     digitalWrite(RED, HIGH);
-     digitalWrite(GREEN, HIGH);
-     digitalWrite(OUTPUT_A, HIGH);
-     digitalWrite(OUTPUT_B, LOW);
-  }
-
-  //PULSE WHITE
-  else if (colourState == 5){
-    //bool quit = false;        CHECK THIS LINE IN NEXT COMMIT
-    digitalWrite(RED, LOW);
-    digitalWrite(GREEN, LOW);
-    digitalWrite(OUTPUT_A, LOW);
-    digitalWrite(OUTPUT_B, HIGH);
-
-    // fade in from min to max in increments of 5 points:
-    for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
-      // sets the value (range from 0 to 255):
-      analogWrite(RED, fadeValue);
-      analogWrite(GREEN, fadeValue);
-      // wait for 50 milliseconds to see the dimming effect
-      delay(50);
-    }
-
-    // fade out from max to min in increments of 5 points:
-    for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) {
-      // sets the value (range from 0 to 255):
-      analogWrite(RED, fadeValue);
-      analogWrite(GREEN, fadeValue);
-      // wait for 50 milliseconds to see the dimming effect
-      delay(50);
-
-      //if (digitalRead(BUTTON) == HIGH) loop();        CHECK THIS LINE AND LINE BELOW IN NEXT COMMMIT
-      loop();
-    }
-
-    analogWrite(RED, 0);
-    analogWrite(GREEN, 0);
-    delay(100);
-    //if (digitalRead(BUTTON) == HIGH) loop(); CHECK THIS LINE IN NEXT COMMIT
-    loop();
-  
-  //colourState++;
-  //if(colourState == MAX_COLOUR_STATES) colourState = 0;
-  }
-
-
-  //RGB SPECTRUM
-  else if (colourState == 6){
-    //bool quit = false; CHECK THIS LINE IN NEXT COMMIT
+//RGB SPECTRUM
+void rgb(){
+  //bool quit = false;
     digitalWrite(RED, LOW);
     digitalWrite(GREEN, LOW);
     digitalWrite(OUTPUT_A, HIGH);
@@ -198,14 +224,9 @@ void toggle_funct(){
       analogWrite(GREEN, fadeValue);
       // wait for 50 milliseconds to see the dimming effect
       delay(50);
-    }
-        //if (digitalRead(BUTTON) == HIGH) quit = true;   CHECK THIS LINE IN NEXT COMMIT
-        loop();
-    
-    //colourState++;
-    //if(colourState == MAX_COLOUR_STATES) colourState = 0;
-  }
+    }    
 }
+
 
 void debounceFunct(){
   if (digitalRead(BUTTON) == HIGH)
